c39f85b11e2939627fa650e284f10c7bae1aab00
AlissonDuarte <alissonsilvamorimduarte@gmail.com>
2024-05-10 19:22:08 -0300

    totp init

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 )
 
 require (
+	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -20,6 +21,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/pquerna/otp v1.4.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/functions/login.go
+++ internal/functions/login.go
@@ -3,7 +3,9 @@ package functions
 import (
 	"clube/infraestructure/models"
 	"fmt"
+	"time"
 
+	"github.com/pquerna/otp/totp"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -22,4 +24,32 @@ func FindUserByEmail(db *gorm.DB, email string) (*models.User, error) {
 
 func VerifyPassword(hash string, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func LoginTotp(user *models.User) error {
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "Clube",
+		AccountName: user.Username,
+	})
+
+	if err != nil {
+		return fmt.Errorf("error to generate totp")
+	}
+
+	fmt.Println("SECRET KEY", key.Secret())
+	now := time.Now()
+	totpCode, err := totp.GenerateCode(key.Secret(), now)
+
+	if err != nil {
+		return fmt.Errorf("cannot access key")
+	}
+
+	valid := totp.Validate(totpCode, key.Secret())
+
+	if valid {
+		return nil
+	} else {
+		return fmt.Errorf("invalid code")
+	}
+
 }
